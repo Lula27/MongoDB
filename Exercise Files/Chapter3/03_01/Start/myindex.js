a8ed4b234da64eb68b8b8b0c86b43c8042e0e026
Loretta Agyemang <loretta.agyemang@gmail.com>
2018-12-21 18:41:09 -0600

    updated json files

---
+++ Exercise Files/Chapter3/03_01/Start/myindex.js
@@ -0,0 +1,14 @@
+// Creating connection to Mongodb using JavaScript 
+
+var MongoClient = require('mongodb').MongoClient; 
+
+// Set up url 
+var url = 'mongodb://localhost:27017/learning_mongo'; 
+
+// Connect ot database 
+
+MongoClient.connect(url, function(err, db) {
+	console.log("Connected successfully to server"); 
+
+	db.close(); 
+}) 
